c2d00fc96a9ffd9dfcb1b0c1ae670aeb944eab70
Vo Van Nghia <vanvnghia@gmail.com>
2022-12-06 21:41:39 +0100

    openposert: remove unused resize_and_merge_gpu

--- include/openposert/net/resize_and_merge.hpp
+++ include/openposert/net/resize_and_merge.hpp
@@ -7,14 +7,6 @@
 namespace openposert {
 
 template <typename T>
-void resize_and_merge_gpu(T* target_ptr,
-                          const std::vector<const T*>& source_ptrs,
-                          const std::array<int, 4>& target_size,
-                          const std::vector<std::array<int, 4>>& source_sizes,
-                          const std::vector<T>& scale_input_to_net_inputs = {
-                              1.f});
-
-template <typename T>
 void resize_and_pad_rbg_gpu(T* target_ptr, const T* const src_ptr,
                             const int source_width, const int source_height,
                             const int target_width, const int target_height,
